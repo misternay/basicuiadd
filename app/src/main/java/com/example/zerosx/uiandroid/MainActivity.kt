62aabfa1d052feda1856434f4ff8dac0ab4837fb
MisterNay <rithikied@hotmail.com>
2018-03-02 16:09:04 +0700

    add login_activite

--- app/src/main/java/com/example/zerosx/uiandroid/MainActivity.kt
+++ app/src/main/java/com/example/zerosx/uiandroid/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.zerosx.uiandroid
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var btn :Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn = findViewById(R.id.btn)
+        btn.setOnClickListener {
+            var xxx = Intent(this,Login::class.java)
+            startActivity(xxx)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+
+        }
     }
+
 }
